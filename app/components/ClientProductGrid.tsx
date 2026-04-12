c8cd7e620a4ddc8285e19091b5f3be776cd95246
deepak.kumar <deepak.kumar@suretekinfosoft.com>
2026-04-12 11:47:22 +0530

    page designed latest.

--- app/components/ClientProductGrid.tsx
+++ app/components/ClientProductGrid.tsx
@@ -249,7 +249,7 @@ const productsWithSlugs = useMemo(() => {
           </h3>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {productsWithSlugs.map((product: Product & { slug: string }) => {
             const imageSrc = getProductImage(product.images);
 
